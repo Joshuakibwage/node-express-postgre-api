2cf2a61bc226da0b445cb79637de711efdfed370
Joshua Kibwage <joshuakibwage23@gmail.com>
2026-03-11 13:45:02 +0300

    feat: add Prisma models for User, Movie, and WatchListItem

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
+import { connectDb, disconnectDb } from "./config/db.js";
 
 //import routes
 import movieRoutes from "./routes/movieRoutes.js";
 
 config();
+connectDb();
 
 const app = express();
 
@@ -15,10 +17,34 @@ app.use("/movies", movieRoutes);
 
 const PORT = 5001;
 
-// app.get("/hello", (req, res) => {
-//     res.json({message: "Hello world"})
-// })
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+//Handle unhandled promise ejections eg.(database connection errors)
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled rejection:", err);
+
+    server.close(async => {
+        await disconnectDb();
+        process.exit(1);
+    })
+})
+
+//Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught exception:", err);
+    await disconnectDB();
+    process.exit(1);
+});
+
+//Graceful shutdown
+process.on("SIGTERM", async () => {
+    console.log("SIGTERM received, shutting down gracefully");
+    server.close(async () => {
+        await disconnectDb();
+        process.exit(0);
+    })
+})
